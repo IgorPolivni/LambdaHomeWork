6d1b41bae4d72dba5efba8cd9c494ce02fabc34f
Poli_tn62@student.itstep.kz <Poli_tn62@student.itstep.kz>
2021-02-14 21:37:35 +0600

    Добавьте файлы проекта.

---
+++ LambdaHomeWork/Button.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegateHomeWork
+{
+
+    public delegate void Handlers(params object[] objects);
+    class Button
+    {
+        public char Border { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Text { get; set; }
+        public ConsoleColor InternalColor { get; set; }
+        public ConsoleColor BorderColor { get; set; }
+        public event Action<object> MyAction;
+
+
+        private void PrintBorderLine()
+        {
+
+            Console.ForegroundColor = BorderColor;
+            for (int i = 0; i < Width; i++)
+                Console.Write(Border);
+
+            Console.WriteLine();
+        }
+
+        private void PrintBorderOrSpace(int index)
+        {
+            if (index == 0 || index == Width - 1)
+                Console.Write(Border);
+            else
+                Console.Write(" ");
+        }
+
+        public void Print()
+        {
+            bool isPrintText = false;
+            PrintBorderLine();
+            ConsoleColor standartColor = Console.ForegroundColor;
+            Console.ForegroundColor = standartColor;
+            for (int i = 0; i < Height; i++)
+            {
+                if (i >= Height / 2 && !isPrintText)
+                {
+                    for (int j = 0; j <= Width; j++)
+                    {
+                        if (j >= Width / 3 && !isPrintText)
+                        {
+                            Console.ForegroundColor = InternalColor;
+                            Console.Write(Text);
+                            Console.ForegroundColor = BorderColor;
+                            j += Text.Length - 1;
+                            isPrintText = true;
+                        }
+                        else
+                        {
+                            PrintBorderOrSpace(j);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < Width; j++)
+                    {
+                        PrintBorderOrSpace(j);
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            PrintBorderLine();
+            Console.ForegroundColor = standartColor;
+        }
+
+    }
+}
---
+++ LambdaHomeWork/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DelegateHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor standartBackGroundColor = Console.BackgroundColor;
+            ConsoleColor standartTextColor = Console.ForegroundColor;
+
+
+            Button button = new Button
+            {
+                Border = '+',
+                Height = 3,
+                Text = "Мир",
+                Width = 15,
+                BorderColor = ConsoleColor.Green,
+                InternalColor = ConsoleColor.Red
+            };
+
+            button.MyAction += x => { MyFirstMethod(x); };
+            button.MyAction += x => { MySecondMethod(x); };
+            button.Print();
+
+            Console.BackgroundColor = standartBackGroundColor;
+            Console.ForegroundColor = standartTextColor;
+        }
+
+        private static void MyFirstMethod(object o) { }
+        private static void MySecondMethod(object o) { }
+    }
+}
